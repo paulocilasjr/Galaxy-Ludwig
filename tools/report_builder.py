a6b03b3bbbd177f26eb7e3fbad07717b78b0fe3e
paulocilasjr <pj_ibm@hotmail.com>
2024-11-07 13:30:48 -0500

    Report draft

---
+++ tools/report_builder.py
@@ -0,0 +1,43 @@
+# REPORT TO DO
+
+#PAGE 1
+    # Setup Parameters
+        ## target column
+        ## seed_number
+        ## Index?
+
+    # Model details
+        # Hyperparameters settings
+        # Evaluation numbers (Accuracy, ROC-AUC, Recall, Precision, F1, Kappa, MCC, PR-AUC-Weighted)
+
+#PAGE 2
+    # ROC-AUC curve plot (training, evaluation, testing)
+    # Confusion Matrix
+    # Threshold plot (precision, recall, f1, tf, queue rate)
+    # Precision-Recall curve
+    # Error - class prediction error
+    # Class Report - precision, recall, f1, support by category
+    # Learning curve
+    # Calibration plot (reliability  curve)
+    # Validation Curve (training score, cross validation score)
+    # Dimension (RadViz for n Features)
+    # Manifold (t-SNE manifold)
+    # RFE - RFECV curve (number of features selected)
+    # Feature importance plot
+    # Feature importance all plot
+
+#PAGE 3
+    # Use gini impurity for calculating feature importance
+    # SHAP summary (feature importance by class)
+
+#PAGE 4
+    # SHAP average impact on predicted response (mean absolute SHAP value)
+    # Permutation importance
+    # pdp plot for each feature
+    # Percentage 1 vs predicted probability
+    # cumulative percentage per category whem sampling top X%
+    # Percentage above and below cutoff
+    # Confusion Matrix
+    # Lift Curve (perfect, model, random)
+    # ROC AUC Curve
+    # PR AUC Curve
